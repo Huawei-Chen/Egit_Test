b366061a6df94791349762d4abe0b023d60678e1
HuaweiChen <773821434@qq.com>
2019-11-05 15:13:07 +0800

    更新1

--- src/com/atguigu/egit/test/HelloEgit.java
+++ src/com/atguigu/egit/test/HelloEgit.java
@@ -3,6 +3,7 @@ package com.atguigu.egit.test;
 public class HelloEgit {
 	public static void main(String[] args) {
 		System.out.println("Hello Egit!!!");
+		System.out.println("update one!!!");
 	}
 
 }
